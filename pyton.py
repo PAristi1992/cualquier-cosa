30791b5128732ad434b1acc662ff7f60a8c0922a
Pablo Aristimuño <p.aristimuno@gmail.com>
2022-01-04 14:26:17 -0300

    Commiteo un .py con 1 variable y dos print

---
+++ pyton.py
@@ -0,0 +1,5 @@
+variable = "esto es un título"
+
+print("\nEl contenido de la variable sin mayúsculas es: " + variable)
+
+print("El contenido de la variable con mayúsculas es: " + variable.title())
